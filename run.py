6bd6b7887c1de354bb384546940dc783cbf523d1
Hirochika Asai <panda@jar.jp>
2023-11-02 16:15:55 +0100

    Performance improvement.

--- run.py
+++ run.py
@@ -74,7 +74,7 @@ def symlink_overwrite(f1, f2):
 Main routine
 """
 def main():
-    xpaths = ['Cisco-IOS-XE-wireless-rrm-oper:rrm-oper-data',
+    xpaths = ['Cisco-IOS-XE-wireless-rrm-oper:rrm-oper-data/rrm-measurement',
               'Cisco-IOS-XE-wireless-access-point-oper:access-point-oper-data/ap-name-mac-map',
               'Cisco-IOS-XE-wireless-access-point-oper:access-point-oper-data/radio-oper-data',
               'Cisco-IOS-XE-wireless-access-point-oper:access-point-oper-data/radio-oper-stats']
@@ -90,9 +90,12 @@ def main():
             for um in msg.update:
                 ## RRM
                 if um.path.elem[0].name == 'Cisco-IOS-XE-wireless-rrm-oper:rrm-oper-data':
-                    jm = json.loads(um.val.json_ietf_val)
-                    jm['timestamp'] = ts
-                    obj_rrm = jm
+                    if um.path.elem[1].name == 'rrm-measurement':
+                        jm = dict(um.path.elem[1].key)
+                        jm.update(json.loads(um.val.json_ietf_val))
+                        if 'rrm-measurement' not in obj_rrm:
+                            obj_rrm['rrm-measurement'] = []
+                        obj_rrm['rrm-measurement'].append(jm)
                 ## AP
                 elif um.path.elem[0].name == 'Cisco-IOS-XE-wireless-access-point-oper:access-point-oper-data':
                     if um.path.elem[1].name == 'ap-name-mac-map':
@@ -114,8 +117,9 @@ def main():
                             obj_ap['radio-oper-stats'] = []
                         obj_ap['radio-oper-stats'].append(jm)
         if ts is not None:
-            ## RRM
             fname = '%d.json' % ts
+            ## RRM
+            obj_rrm['timestamp'] = ts
             js = json.dumps(obj_rrm, indent=2)
             file_path = '%s/rrm/%s' % (databasedir, fname)
             rrm_path = '%s/rrm.json' % databasedir
